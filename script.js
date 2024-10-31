ee7c74a3c258050e3d18e77fa69dc61505651b1a
eddie533 <179413059+eddie533@users.noreply.github.com>
2024-10-31 00:07:28 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    if ('loading' in HTMLImageElement.prototype) {
+        console.log('Browser supports lazy-loading');
+    } else {
+        console.log('Browser does not support lazy-loading');
+        const lazyImages = document.querySelectorAll('img[loading="lazy"]');
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    img.src = img.dataset.src;
+                    img.removeAttribute('loading');
+                    observer.unobserve(img);
+                }
+            });
+        });
+
+        lazyImages.forEach(img => {
+            img.dataset.src = img.src;
+            img.src = '';
+            observer.observe(img);
+        });
+    }
+});
+
